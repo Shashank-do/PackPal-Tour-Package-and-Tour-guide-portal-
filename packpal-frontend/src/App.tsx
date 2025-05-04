5c632110e3b178a22b231db21ee064bf2ceb9551
Shashank-do <shashankmuraleedhar@gmail.com>
2025-05-04 10:13:03 +0530

    Add files via upload

---
+++ packpal-frontend/src/App.tsx
@@ -0,0 +1,41 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Destinations from "./pages/Destinations";
+import ToursPackages from "./pages/ToursPackages";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PackageDetails from "./pages/PackageDetails";
+import BookingSuccess from "./pages/BookingSuccess";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/destinations" element={<Destinations />} />
+          <Route path="/tours-packages" element={<ToursPackages />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/package/:id" element={<PackageDetails />} />
+          <Route path="/booking-success" element={<BookingSuccess />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
